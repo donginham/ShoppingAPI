1b173c8c76074fff288bec90c7d36ad963bb8edc
HDI <hdihdi1869@gmail.com>
2025-07-27 21:13:26 +0900

    깃 환경변경 확인용

--- ShoppingAPI/Deco&Cell/ResultCollectionViewCell.swift
+++ ShoppingAPI/Deco&Cell/ResultCollectionViewCell.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 import Kingfisher
+import SnapKit
 class ResultCollectionViewCell: UICollectionViewCell {
     static let identifier = "ResultCollectionViewCell"
+    let objects:[String] = ["itemImage","mallLabel","itemTitle","itemPrice"]
     let itemImage = {
         let itemImage = UIImageView()
         return itemImage
@@ -27,12 +29,23 @@ class ResultCollectionViewCell: UICollectionViewCell {
     }()
     func setupCell (shopData:shopData) {
         setupLayout(result: shopData)
+        itemImage.snp.makeConstraints { make in
+            make.centerX.equalTo(contentView)
+            make.verticalEdges.equalTo(contentView).inset(10)
+            make.height.equalTo(160)
+            
+        }
     }
 }
 private extension ResultCollectionViewCell {
     func setupLayout(result: shopData) {
+        contentView.addSubview(itemImage)
+        contentView.addSubview(itemPrice)
+        contentView.addSubview(<#T##view: UIView##UIView#>)
         itemTitle.text = result.title
         mallLabel.text = result.mallName
+        itemPrice.text  = result.lprice
+        
         itemImage.kf.setImage(with: URL(string: result.image))
     }
 }
